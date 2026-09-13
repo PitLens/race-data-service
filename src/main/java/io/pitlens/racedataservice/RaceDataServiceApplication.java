ff365384c5c6a06540c9f2596e21a1844c6801b1
Stepan Omeliukh <stepan-omeliukh@Stepans-MacBook-Pro.local>
2026-09-13 03:45:46 +0300

    Add other files

---
+++ src/main/java/io/pitlens/racedataservice/RaceDataServiceApplication.java
@@ -0,0 +1,13 @@
+package io.pitlens.racedataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaceDataServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RaceDataServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/pitlens/racedataservice/RaceDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.pitlens.racedataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RaceDataServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
